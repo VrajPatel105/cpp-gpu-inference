f9720caa2ee144fa8dda66d32bd74ff4799698a3
Vraj Patel <vrajp1052005@gmail.com>
2026-05-07 22:46:45 -0400

    basic code

---
+++ 1. cpp core/basic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string my_fav_color;
+    cout << "Enter your favourite color Vraj :) " << endl;
+    getline(cin, my_fav_color);
+    cout << "Vraj fav color is " << my_fav_color << endl;
+    return 0;
+}

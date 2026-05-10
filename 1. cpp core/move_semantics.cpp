7f0f93eb5b12357c2c6cc1379ade41674a4a8d76
Vraj Patel <vrajp1052005@gmail.com>
2026-05-10 15:41:49 -0400

    move semantics

---
+++ 1. cpp core/move_semantics.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class String{
+
+public:
+    String() = default;
+    String(const char* string)
+    {
+        printf("Created \n");
+        m_Size = strlen(string);
+        m_Data = new char[m_Size];
+        memcpy(m_Data, string, m_Size);
+    }
+
+    ~String()
+    {
+        delete m_Data;
+    }
+
+private:
+    char* m_Data;
+    uint32_t m_Size;
+};
+
+class Entity
+{
+public:
+    Entity(const String& name)
+        : m_Name(name)
+    {}
+
+private:
+    string m_Name;
+};
+
+
+int main(){
+
+    Entity entity(String("Vraj"));
+
+    return 0;
+}
--- 1. cpp core/smartpointers.cpp
+++ 1. cpp core/smartpointers.cpp
@@ -14,36 +14,36 @@ public:
 };
 
 int main() {
-    // unique_ptr<int> unPtr1 = make_unique<int>(25);
-    // // cout << unPtr1.get() << endl; // address
-    // // cout << *unPtr1 << endl; // dereferencing the pointer
+    unique_ptr<int> unPtr1 = make_unique<int>(25);
+    // cout << unPtr1.get() << endl; // address
+    // cout << *unPtr1 << endl; // dereferencing the pointer
 
-    // unique_ptr<int> unPtr2 = move(unPtr1); // moving the ownership.
+    unique_ptr<int> unPtr2 = move(unPtr1); // moving the ownership.
 
-    // cout << unPtr2.get() << endl;
-    // cout << unPtr1.get() << endl;
-    // // output : 
-    // // 0x1bb30d06310
+    cout << unPtr2.get() << endl;
+    cout << unPtr1.get() << endl;
+    // output : 
+    // 0x1bb30d06310
     // 0
 
-    // unique_ptr<Myclass>unPtr1 = make_unique<Myclass>(); // the unique pointer will be dealocatted when the scope ends.
+    unique_ptr<Myclass>unPtr1 = make_unique<Myclass>(); // the unique pointer will be dealocatted when the scope ends.
 
-    // {
-    //     unique_ptr<Myclass>unPtr2 = make_unique<Myclass>();
-    // } // this will end when the code reaches line 33 {end of the scope}
+    {
+        unique_ptr<Myclass>unPtr2 = make_unique<Myclass>();
+    } // this will end when the code reaches line 33 {end of the scope}
 
     // shared pointer
-    // shared_ptr<Myclass>shPtr1 = make_unique<Myclass>();
-    // cout << "Shared count : " << shPtr1.use_count() << endl;
-    // shared_ptr<Myclass>shPtr2 = shPtr1;
-    // cout << "Shared count : " << shPtr2.use_count() << endl;
-    // // lets put one of them in a scope
-    // {
-    //     shared_ptr<Myclass>shPtr3 = shPtr1;
-    //     cout << "Shared count : " << shPtr3.use_count() << endl;
-    // }
+    shared_ptr<Myclass>shPtr1 = make_unique<Myclass>();
+    cout << "Shared count : " << shPtr1.use_count() << endl;
+    shared_ptr<Myclass>shPtr2 = shPtr1;
+    cout << "Shared count : " << shPtr2.use_count() << endl;
+    // lets put one of them in a scope
+    {
+        shared_ptr<Myclass>shPtr3 = shPtr1;
+        cout << "Shared count : " << shPtr3.use_count() << endl;
+    }
 
-    // cout << "Shared count : " << shPtr1.use_count() << endl;
+    cout << "Shared count : " << shPtr1.use_count() << endl;
 
     // Weak Pointer : will not keep the object alive when it's last owner leave's it's score
     weak_ptr<int> wePtr1;

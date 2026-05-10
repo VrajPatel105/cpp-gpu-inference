e88a251cad305a1d6abd3c2a99ce471a38d94105
Vraj Patel <vrajp1052005@gmail.com>
2026-05-09 22:26:12 -0400

    header file

---
+++ README.md
@@ -0,0 +1,1 @@
+# cpp-gpu-inference
---
+++ header.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "adder.h"
+
+int main(){
+    int x = 3;
+    int y = 5;
+    int result = addNum(x,y);
+    cout << result << endl;
+}

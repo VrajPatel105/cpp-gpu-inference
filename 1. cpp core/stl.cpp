50b406205c73e63c3f10bf9f7409b4d0053fe851
unknown <vrajp1052005@gmail.com>
2026-05-14 21:00:56 -0400

    stl task 1 practice

---
+++ 1. cpp core/stl.cpp
@@ -0,0 +1,42 @@
+// revisiting stl by doing some tasks : 
+// 1. Reads 10 numbers from stdin into a std::vector<int>
+// 2. Sorts them descending using std::sort with a lambda comparator
+// 3. Counts how many are even using std::count_if with another lambda
+// 4. Stores frequencies in a std::unordered_map<int, int>
+// 5. Prints the map using a range-based for loop
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Helper {
+public:
+    // print vector function
+    void PrintVec(vector<int> v){
+        for(int i = 0; i<v.size(); i++){
+        cout << v[i] << endl;
+        }
+    }
+};
+
+// main method : 
+int main(){
+
+// obj for the helper class
+Helper h;
+
+// 1. Reading 10 numbers from stdin into a std::vector<int>
+    vector<int> user_input;
+    // get user input 
+    for(int i = 0; i<10; i++){
+        int current_user_input;
+        cin >> current_user_input;
+        user_input.push_back(current_user_input);
+    }
+
+    cout << "printing the stored vector values that were entrerd by the user" << endl;
+    // once done, print the vector
+    h.PrintVec(user_input);
+    
+    return 0;
+}

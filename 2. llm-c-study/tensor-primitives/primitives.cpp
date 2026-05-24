0deb7486de6674bb71073deb3f1f5823e337d412
unknown <vrajp1052005@gmail.com>
2026-05-24 01:36:25 -0400

    adding cmake to tensor primitives

---
+++ 2. llm-c-study/tensor-primitives/primitives.cpp
@@ -0,0 +1,10 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(myprims, m) {
+    m.def("add", &add, "A function that adds two numbers");
+}

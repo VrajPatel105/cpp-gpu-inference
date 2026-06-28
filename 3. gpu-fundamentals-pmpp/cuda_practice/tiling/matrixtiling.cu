b90e664da6731c688b6e46ac411ad1dfa5238336
VrajPatel105 <vrajp1052005@gmail.com>
2026-06-28 04:07:05 -0400

    writing the whole kernel

--- 3. gpu-fundamentals-pmpp/cuda_practice/tiling/matrixtiling.cu
+++ 3. gpu-fundamentals-pmpp/cuda_practice/tiling/matrixtiling.cu
@@ -16,9 +16,48 @@
 #include <cuda.h>
 using namespace std;
 
+#define TILE_WIDTH 2
 
 
 // writing the main kernel
+
+__global__ void optimizedMatmul(float* A, float* B, float *P, int N){
+
+    // defining the variables for easy naming
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int row = by * TILE_WIDTH + ty;
+    int col = bx * TILE_WIDTH + tx;
+
+    // declaing the block's scratchpads and the tiling
+    __shared__ float Mds[TILE_WIDTH][TILE_WIDTH]; // blocks scratchpad
+    __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
+
+    // Mds and Nds are shared tiles
+    // pValue is the accumulator
+    // ph walks the phases
+
+    float Pvalue = 0.0f; // the running total
+
+    for(int ph = 0; ph < N / TILE_WIDTH; ++ph){ // which tile am I on currently
+        // load, sync, multiply, sync 
+        // 1. load my one M element and my one N element into the shared tile
+        Mds[ty][tx] = A[row*N + (ph*TILE_WIDTH + tx)];
+        Nds[ty][tx] = B[(ph*TILE_WIDTH + ty)*N + col];
+        __syncthreads();
+        // 3. k-loop: Pvalue += Mds[ty][k] * Nds[k][tx];  ---> the dot product from the tile
+        for (int k = 0; k < TILE_WIDTH; ++k) {
+            Pvalue += Mds[ty][k] * Nds[k][tx];
+        }
+        __syncthreads();
+
+    }
+    P[row*N + col] = Pvalue;;
+
+}
 
 
 // writing the main 
@@ -26,6 +65,7 @@ using namespace std;
 int main(){
 
     // lets first define the arrays
+    int N = 4;
     float arrA[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, *h_A = arrA;
     float arrB[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, *h_B = arrB;
     float arrC[16], *h_C = arrC; // this is the resulting array.
@@ -53,6 +93,10 @@ int main(){
 
     
     // define the blocks and threads and then caLL the kernel
+    
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
+    dim3 gridDim(N / TILE_WIDTH, N / TILE_WIDTH);
+    optimizedMatmul<<<gridDim, blockDim>>>(d_A, d_B, d_C, N);
 
 
 }

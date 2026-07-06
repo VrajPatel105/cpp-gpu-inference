c01ac981ff0a9acf351fadc9b5fec5bad4e95bd5
VrajPatel105 <vrajp1052005@gmail.com>
2026-07-06 19:41:09 -0400

    scaling naive matmul

--- 3. gpu-fundamentals-pmpp/cuda_practice/matmul_optimization/matmul.cu
+++ 3. gpu-fundamentals-pmpp/cuda_practice/matmul_optimization/matmul.cu
@@ -1,75 +1,96 @@
 #include <cuda.h>
+#include <cuda_runtime.h>
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 __global__ void matmul(float *A, float *B, float *C, int N){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
+
     if(row < N && col < N){
         float sum = 0.0f;
         for(int k = 0; k < N; k++){
-            sum += A[row*N + k] * B[k*N + col];
+            sum += A[row * N + k] * B[k * N + col];
         }
-        C[row*N + col] = sum;
+        C[row * N + col] = sum;
     }
 }
 
 int main(){
+    int N = 1024;
+    size_t size = N * N;
+    size_t bytes = size * sizeof(float);
 
-    int N = 4;
-    float arrA[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, *h_A = arrA;
-    float arrB[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, *h_B = arrB;
-    float arrC[16], *h_C = arrC; 
+    float *h_A = new float[size];
+    float *h_B = new float[size];
+    float *h_C = new float[size];
+
+    srand(time(NULL));
+
+    for(size_t i = 0; i < size; i++){
+        h_A[i] = rand() % 10 + 1;
+        h_B[i] = rand() % 10 + 1;
+    }
 
     float *d_A, *d_B, *d_C;
 
-    if(cudaMalloc(&d_A, sizeof(float) * 16) != cudaSuccess){
+    if(cudaMalloc(&d_A, bytes) != cudaSuccess){
         cout << "Cuda Mem for d_A not allocated" << endl;
         return 0;
     }
-    if(cudaMalloc(&d_B, sizeof(float) * 16) != cudaSuccess){
+    if(cudaMalloc(&d_B, bytes) != cudaSuccess){
         cout << "Cuda Mem for d_B not allocated" << endl;
         return 0;
     }
-    if(cudaMalloc(&d_C, sizeof(float) * 16) != cudaSuccess){
+    if(cudaMalloc(&d_C, bytes) != cudaSuccess){
         cout << "Cuda Mem for d_C not allocated" << endl;
         return 0;
     }
 
-    if(cudaMemcpy(d_A, h_A, sizeof(float) * 16, cudaMemcpyHostToDevice) != cudaSuccess){
-        cout << "Failed to copy memory from d_A to h_A" << endl;
+    if(cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice) != cudaSuccess){
+        cout << "Failed to copy memory from h_A to d_A" << endl;
         return 0;
     }
-    if(cudaMemcpy(d_B, h_B, sizeof(float) * 16, cudaMemcpyHostToDevice) != cudaSuccess){
-        cout << "Failed to copy memory from d_B to h_B" << endl;
+    if(cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice) != cudaSuccess){
+        cout << "Failed to copy memory from h_B to d_B" << endl;
         return 0;
     }
 
-    dim3 threadsPerBlock(16,16);
-    dim3 numBlocks((N+15)/16, (N+15)/16);
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks((N + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
     matmul<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, N);
-    
-    cudaMemcpy(h_C, d_C, sizeof(float) * 16, cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
 
-    // print
-    cout << "Pasting the output " << endl;
+    float ms = 0.0f;
+    cudaEventElapsedTime(&ms, start, stop);
 
-    for(int i = 0; i<16; i++){
-        cout << h_C[i] << endl;
-    }
+    cudaMemcpy(h_C, d_C, bytes, cudaMemcpyDeviceToHost);
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << h_C[i*N + j] << "\t";
-        }
-        cout << endl;
-    }
+    cout << "Kernel time: " << ms << " ms" << endl;
+    cout << "C[0][0] = " << h_C[0] << endl;
+    cout << "C[N-1][N-1] = " << h_C[N * N - 1] << endl;
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
 
     return 0;
 }

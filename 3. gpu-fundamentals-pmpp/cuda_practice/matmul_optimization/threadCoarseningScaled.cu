7b459dfb89fad458e3e683e382960c1be9202ed2
VrajPatel105 <vrajp1052005@gmail.com>
2026-07-06 20:04:00 -0400

    thread coarsening scaled

--- 3. gpu-fundamentals-pmpp/cuda_practice/matmul_optimization/threadCoarseningScaled.cu
+++ 3. gpu-fundamentals-pmpp/cuda_practice/matmul_optimization/threadCoarseningScaled.cu
@@ -2,7 +2,7 @@
 #include <cuda.h>
 using namespace std;
 
-#define TILE_WIDTH 2
+#define TILE_WIDTH 16
 #define COARSE_FACTOR 2 
 
 __global__ void coarsenedMatrixTiling(float* A, float* B, float* P, int N){
@@ -49,31 +49,83 @@ __global__ void coarsenedMatrixTiling(float* A, float* B, float* P, int N){
 }
 
 int main(){
+    int N = 1024;
+    size_t size = N * N;
+    size_t bytes = size * sizeof(float);
 
-    int N = 4;
-    float arrA[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, *h_A = arrA;
-    float arrB[16] = {1,2,3,4, 5,6,7,8, 9,10,11,12, 13,14,15,16}, *h_B = arrB;
-    float resultArr[16], *h_result = resultArr;
+    float *h_A = new float[size];
+    float *h_B = new float[size];
+    float *h_C = new float[size];
 
-    float *d_A, *d_B, *d_result;
-
-    cudaMalloc(&d_A, sizeof(float) * 16);
-    cudaMalloc(&d_B, sizeof(float) * 16);
-    cudaMalloc(&d_result, sizeof(float) * 16);
-
-    cudaMemcpy(d_A, h_A, sizeof(float) * 16, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, sizeof(float) * 16, cudaMemcpyHostToDevice);
-
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
-    
-    dim3 gridDim(N / (TILE_WIDTH * COARSE_FACTOR), N / TILE_WIDTH);
-    coarsenedMatrixTiling<<<gridDim, blockDim>>>(d_A, d_B, d_result, N);
-
-    cudaMemcpy(h_result, d_result, sizeof(float) * 16, cudaMemcpyDeviceToHost);
-
-    for(int i = 0; i < 16; ++i){
-        cout << "i = " << i << " -> " << h_result[i] << endl;
+    for(size_t i = 0; i < size; i++){
+        h_A[i] = 1.0f;
+        h_B[i] = 1.0f;
     }
+
+    float *d_A, *d_B, *d_C;
+
+    if(cudaMalloc(&d_A, bytes) != cudaSuccess){
+        cout << "Cuda Mem for d_A not allocated" << endl;
+        return 0;
+    }
+    if(cudaMalloc(&d_B, bytes) != cudaSuccess){
+        cout << "Cuda Mem for d_B not allocated" << endl;
+        return 0;
+    }
+    if(cudaMalloc(&d_C, bytes) != cudaSuccess){
+        cout << "Cuda Mem for d_C not allocated" << endl;
+        return 0;
+    }
+
+    if(cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice) != cudaSuccess){
+        cout << "Failed to copy memory from h_A to d_A" << endl;
+        return 0;
+    }
+    if(cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice) != cudaSuccess){
+        cout << "Failed to copy memory from h_B to d_B" << endl;
+        return 0;
+    }
+
+    dim3 threadsPerBlock(TILE_WIDTH, TILE_WIDTH);
+    dim3 numBlocks(N/(TILE_WIDTH*COARSE_FACTOR), N/(TILE_WIDTH*COARSE_FACTOR));
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+    coarsenedMatrixTiling<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, N);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float ms = 0.0f;
+    cudaEventElapsedTime(&ms, start, stop);
+
+    cudaMemcpy(h_C, d_C, bytes, cudaMemcpyDeviceToHost);
+
+    cout << "Kernel time: " << ms << " ms" << endl;
+    cout << "C[0][0] = " << h_C[0] << endl;
+    cout << "C[N-1][N-1] = " << h_C[N * N - 1] << endl;
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
 
     return 0;
 }
+
+// output 
+
+// vrajpatel@Vraj:/mnt/c/My Projects/Deep Learning Projects/cpp-gpu-inference/3. gpu-fundamentals-pmpp/cuda_practice$ nvcc matmul_optimization/threadCoarseningScaled.cu -o build/thread_coarsening_scaled
+// vrajpatel@Vraj:/mnt/c/My Projects/Deep Learning Projects/cpp-gpu-inference/3. gpu-fundamentals-pmpp/cuda_practice$ ./build/thread_coarsening_scaled
+
+// Kernel time: 1.20531 ms
+// C[0][0] = 1024
+// C[N-1][N-1] = 0

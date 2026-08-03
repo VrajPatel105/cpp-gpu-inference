96baac3db3eed932b8a166f4da32c40144c6e19a
VrajPatel105 <vrajp1052005@gmail.com>
2026-08-02 20:47:52 -0400

    final triton naive matmul kernel

--- 4. trition-kernels/matmul/naive_matmul.py
+++ 4. trition-kernels/matmul/naive_matmul.py
@@ -5,6 +5,31 @@ import triton.language as tl
 import torch
 
 # kernel here : 
+@triton.jit
+def naive_matmul_kernel(
+    A_ptr, B_ptr, C_ptr,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn
+):
+
+    pid = tl.program_id(axis=0)
+
+    m = pid // N
+    n = pid % N
+
+    if (m >= M) or (n >= N):
+        return
+
+    accumulator = 0.0
+    for k in range(0,K):
+        a_val = tl.load(A_ptr + m * stride_am + k * stride_ak)
+        b_val = tl.load(B_ptr + n * stride_bn + k * stride_bk)
+        accumulator += a_val * b_val
+
+    tl.store(C_ptr + m * stride_cm + n * stride_cn, accumulator)
+
 
 
 def naive_matmul(A,B):
@@ -13,7 +38,7 @@ def naive_matmul(A,B):
 
     assert K == K2
 
-    C = torch.zeroes((M,N), device=A.device, dtype=A.dtype)
+    C = torch.zeros((M,N), device=A.device, dtype=A.dtype)
 
     grid = (M*N,)
 
@@ -26,3 +51,8 @@ def naive_matmul(A,B):
     )
 
     return C
+
+A = torch.randn(6,8, device="cuda")
+B = torch.randn(8,4, device="cuda")
+
+print(naive_matmul(A,B))

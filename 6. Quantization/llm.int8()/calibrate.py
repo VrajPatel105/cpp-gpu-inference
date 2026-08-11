1b73934a780bf2cba1b36eb35a76cca2a42404f9
VrajPatel105 <vrajp1052005@gmail.com>
2026-08-11 04:33:10 -0400

    writing the main flagging logic

--- 6. Quantization/llm.int8()/calibrate.py
+++ 6. Quantization/llm.int8()/calibrate.py
@@ -64,9 +64,9 @@ model = model.to(device)
 calibrate(model, val_loader, device, pad_id)
 
 
-print(f"Calibrated {len(running_max)} layers")
-for name, vals in list(running_max.items())[:3]:
-    print(name, vals.shape, vals.max().item())
+# print(f"Calibrated {len(running_max)} layers")
+# for name, vals in list(running_max.items())[:3]:
+#     print(name, vals.shape, vals.max().item())
 
 # output for aboev loop
 # (mlenv) vraj@Vraj:/mnt/c/dev/projects/cpp-gpu-inference/6. Quantization/llm.int8()$ python calibrate.py
@@ -75,3 +75,69 @@ for name, vals in list(running_max.items())[:3]:
 # encoder_blocks.0.multi_head_attention.W_k torch.Size([512]) 101.70893859863281
 # encoder_blocks.0.multi_head_attention.W_v torch.Size([512]) 101.70893859863281
 # (mlenv) vraj@Vraj:/mnt/c/dev/projects/cpp-gpu-inference/6. Quantization/llm.int8()$ 
+
+
+# now we get the final block here which is to find out which channels from the layers
+# that are stored in the running_max dict have value above a threshold value
+# we will use alpha = 6 as implemented in the llm.int8() paper.
+
+# iterating through the dict
+
+outlier_dict = {}
+
+# for key, value in list(running_max.items()):
+#     mask = (value > 6.0).to(device)
+#     outlier_indices = torch.arange(value.numel(), device=mask.device)[mask]
+#     outlier_dict[key] = outlier_indices
+
+# key = "encoder_blocks.0.multi_head_attention.W_k"
+# print("\n" + key )
+# print(running_max[key].min().item())
+# print(running_max[key].max().item())
+# print(running_max[key].mean().item())
+
+# key = "encoder_blocks.1.multi_head_attention.W_k"
+# print("\n" + key )
+# print(running_max[key].min().item())
+# print(running_max[key].max().item())
+# print(running_max[key].mean().item())
+
+
+# findings  
+
+# encoder_blocks.0.multi_head_attention.W_k
+# 58.67816925048828
+# 101.70893859863281
+# 76.09907531738281
+
+# encoder_blocks.1.multi_head_attention.W_k
+# 2.6325955390930176
+# 4.541968822479248
+# 3.359626293182373
+
+# upon priting the first layer vector in outlier_indices dict, i found out that all the indices from 0 to 511 were getting flagged as outlier
+# this was because all the values in that block were higher than the flag, which is alpha = 6 here (we took this from teh paper)
+# but upon priting the values for the block1, the values are well within the range but then no channels are getting flagged in this blcok since there's no value greater than the alpha 
+# as you can notice here, this means, we will have to implement dynamic alpha value per block rather than a standard global value
+
+# here based on stats, i will be using this value : alpha = mean + 3*std -> we can easily term this value as outlier if it's well greater than 3 times it's std + mean
+
+# modified new loop with per block alpha
+
+for key, value in list(running_max.items()):
+    alpha = torch.mean(value) + 3 * torch.std(value)
+    mask = (value > alpha).to(device)
+    outlier_indices = torch.arange(value.numel(), device=mask.device)[mask]
+    outlier_dict[key] = outlier_indices
+
+torch.save({
+    'running_max': running_max,
+    'outlier_dict': outlier_dict,
+}, 'calibration_data.pt')
+
+# for name, vals in list(outlier_dict.items())[82:85]:
+#     print(name, vals)
+# output for thsi loop
+# decoder_blocks.4.cross_attention.W_v tensor([127, 318, 419, 475, 501], device='cuda:0')
+# decoder_blocks.4.cross_attention.W_o tensor([127, 219, 461, 478], device='cuda:0')
+# decoder_blocks.4.feed_forward.linear1 tensor([ 24, 131, 161, 330, 507], device='cuda:0')

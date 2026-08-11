da12a6ccf065745dd1e8adae32d0ba42a27a873d
VrajPatel105 <vrajp1052005@gmail.com>
2026-08-10 22:54:25 -0400

    getting masks functions in utils

--- en-de-transformer/train.py
+++ en-de-transformer/train.py
@@ -4,6 +4,7 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader, random_split
 from model import build_transformer
 import torch.optim as optim
+from utils import make_masks, causal_mask
 
 from tokenizer import Tokenizer
 
@@ -130,19 +131,6 @@ model = model.to(device)
 #     10. Accumulate loss for reporting
 # Then wrap all of that in an outer for epoch in range(num_epochs): loop.
 
-def make_masks(encoder_input, decoder_input, pad_id, device):
-  
-  tgt_len = decoder_input.size(1)
-  src_mask = (encoder_input != pad_id).unsqueeze(1).unsqueeze(1)
-  tgt_pad_mask = (decoder_input != pad_id).unsqueeze(1).unsqueeze(1)
-  causal_m = torch.tril(torch.ones(tgt_len, tgt_len, device=device)).bool()
-  tgt_mask = (tgt_pad_mask & causal_m).int()
-
-  return src_mask.int(), tgt_mask
-
-# just making another casual mask seperate function that will be in help during translate()
-def causal_mask(size, device):
-    return torch.tril(torch.ones(size, size, device=device)).int().unsqueeze(0).unsqueeze(0)
 
 def evaluate(model, loader, criterion, device, pad_id):
     model.eval()
